38b484c3c1bc97c46e1e91fd5633eedca47c9248
MustaphaBousilDev <bousilmustapha@gmail.com>
2026-03-09 02:47:23 +0000

    add config of service discovery

--- nexora-gateway/src/main/java/com/nexora_gateway/nexora_gateway/NexoraGatewayApplication.java
+++ nexora-gateway/src/main/java/com/nexora_gateway/nexora_gateway/NexoraGatewayApplication.java
@@ -2,23 +2,14 @@ package com.nexora_gateway.nexora_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NexoraGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NexoraGatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoute(RouteLocatorBuilder builder) {
-		return builder.routes().route(p -> p
-				.path("/get")
-				.filters(f -> f.addRequestHeader("Hello", "World"))
-				.uri("http://httpbin.org:80")).build();
 	}
 
 }
